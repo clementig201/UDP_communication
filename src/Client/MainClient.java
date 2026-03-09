51653f9e6cdecc913262c69d2d71ae40180a1c88
clementig201 <clementig201@gmail.com>
2026-03-09 12:24:58 +0100

    1

--- src/Client/MainClient.java
+++ src/Client/MainClient.java
@@ -1,15 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+package Client;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.*;
+
+public class MainClient {
+    public static void main(String[] args) {
+
+        try {
+            InetAddress serverAddress = InetAddress.getLocalHost();
+            System.out.println("Indirizzo del server trovato!");
+            DatagramSocket dSocket = new DatagramSocket();
+            String message = "ciao";
+            int port = 3000;
+            DatagramPacket outPacket = new DatagramPacket(message.getBytes(), message.length(), serverAddress, port);
+            dSocket.send(outPacket);
+            byte[] buffer = new byte[256];
+            DatagramPacket inPacket = new DatagramPacket(buffer, buffer.length);
+            dSocket.receive(inPacket);
+            String response = new String(inPacket.getData(), 0, inPacket.getLength());
+            dSocket.close();
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        }catch (IOException e){
+            throw new RuntimeException(e);
         }
     }
-}
+    }
--- src/Server/MainServer.java
+++ src/Server/MainServer.java
@@ -1,4 +1,31 @@
 package Server;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
 public class MainServer {
+    public static void main(String[] args) {
+        try {
+            int port = 3000;
+            DatagramSocket dSocket = new DatagramSocket(port);
+            System.out.println("Apertura porta in corso!");
+            byte[] bufferIn = new byte[256];
+            DatagramPacket inPacket = new DatagramPacket(bufferIn, bufferIn.length);
+            dSocket.receive(inPacket);//metodo bloccante
+            System.out.println("Ricezione effettuata");
+            String message = new String (inPacket.getData(), inPacket.getLength());
+            InetAddress clientAddress = inPacket.getAddress();
+            int p = inPacket.getPort();
+            DatagramPacket output = new DatagramPacket(message.getBytes(), message.length(), clientAddress, p);
+            dSocket.send(output);
+
+        }catch (SocketException e){
+          throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }

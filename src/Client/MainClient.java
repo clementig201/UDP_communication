9d418d3a2732975c3d169f63c69aa0aa21273ad0
clementig201 <clementig201@gmail.com>
2026-03-16 11:49:46 +0100

    2

--- src/Client/MainClient.java
+++ src/Client/MainClient.java
@@ -8,21 +8,28 @@ public class MainClient {
     public static void main(String[] args) {
 
         try {
-            InetAddress serverAddress = InetAddress.getLocalHost();
+            InetAddress serverAddress = InetAddress.getLocalHost();//1 prendiamo l' indirizzo del server
             System.out.println("Indirizzo del server trovato!");
-            DatagramSocket dSocket = new DatagramSocket();
-            String message = "ciao";
+            DatagramSocket dSocket = new DatagramSocket();//2 instanzio una datagram socket
+            String message = "ciao";//3 scrivo un messaggio
             int port = 3000;
             DatagramPacket outPacket = new DatagramPacket(message.getBytes(), message.length(), serverAddress, port);
-            dSocket.send(outPacket);
+            dSocket.send(outPacket);//4 spedisco il messaggio
+
+            // 1 ricevo le informazioni nel buffer in
             byte[] buffer = new byte[256];
+            // 2 decido il nome del datagram packet
             DatagramPacket inPacket = new DatagramPacket(buffer, buffer.length);
+            // 3 metodo bloccante
             dSocket.receive(inPacket);
-            String response = new String(inPacket.getData(), 0, inPacket.getLength());
+            // 4 stampo che ho ricevuto i messagi
+            System.out.println("Ricezione effettuata" + inPacket);
             dSocket.close();
-        } catch (SocketException e) {
-            throw new RuntimeException(e);
-        }catch (IOException e){
+        } catch (UnknownHostException e) {
+            System.err.println("non trovo l'indirizzo del server");
+        } catch(SocketException e){
+            System.err.println("socket non trovato");
+        } catch (IOException e){
             throw new RuntimeException(e);
         }
     }
--- src/Server/MainServer.java
+++ src/Server/MainServer.java
@@ -1,29 +1,35 @@
 package Server;
 
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
+import java.net.*;
 
 public class MainServer {
     public static void main(String[] args) {
         try {
             int port = 3000;
             DatagramSocket dSocket = new DatagramSocket(port);
+            // è un server perché lo instanziamo con una porta quindi le primitive sono socket() e bind()
             System.out.println("Apertura porta in corso!");
+            // 1 ricevo le informazioni nel buffer in
             byte[] bufferIn = new byte[256];
+            // 2 decido il nome del datagram packet
             DatagramPacket inPacket = new DatagramPacket(bufferIn, bufferIn.length);
-            dSocket.receive(inPacket);//metodo bloccante
+            // 3 metodo bloccante
+            dSocket.receive(inPacket);
+            // 4 stampo che ho ricevuto i messagi
             System.out.println("Ricezione effettuata");
+
+            //1 prendiamo l' indirizzo del client
             String message = new String (inPacket.getData(), inPacket.getLength());
             InetAddress clientAddress = inPacket.getAddress();
             int p = inPacket.getPort();
+            //2 instanzio una datagram socket
             DatagramPacket output = new DatagramPacket(message.getBytes(), message.length(), clientAddress, p);
+            //3 spedisco il messaggio output
             dSocket.send(output);
 
-        }catch (SocketException e){
-          throw new RuntimeException(e);
+        }catch (BindException e){
+          System.err.println("porta già in uso");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
